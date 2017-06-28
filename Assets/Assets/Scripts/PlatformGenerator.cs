fc2da31946bf7fe2b96542e733d07e04151757bf
Ivan <ivan.ng.sm99@gmail.com>
2017-06-28 14:32:15 +0800

    Assets + proj settings

---
+++ Assets/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject platform;
+    public Transform generationPt;
+    public float distBetween;
+
+    private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+        //width of platform's box collider
+        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Spawnfloor();
+
+    }
+
+    void Spawnfloor()
+    {
+        if(transform.position.x < generationPt.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distBetween, transform.position.y, transform.position.z);
+
+            Instantiate(platform, transform.position, transform.rotation);
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    bool mainWeapon = true;
+    bool Pressed = false;
+    [SerializeField]
+    float bulletCooldown = .25f;
+    float Timer = .25f;
+    [SerializeField]
+    float bulletSpeed = 2f;
+
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    Rigidbody2D bulletRB;
+    [SerializeField]
+    Transform bulletSpawn;
+
+    private void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+
+        if (Timer > bulletCooldown && Pressed)
+        {
+            Rigidbody2D rb;
+
+            animator.SetBool("Shoot", true);
+            rb = Instantiate(bulletRB, bulletSpawn.position, bulletSpawn.rotation);
+
+            rb.AddForce(bulletSpeed * bulletSpawn.right);
+
+            Timer = 0;
+        }
+        else if (!Pressed)
+        {
+            animator.SetBool("Shoot", false);
+        }
+
+        Debug.Log(Pressed);
+    }
+
+    void OnEnable()
+    {
+        LeanTouch.OnFingerDown += press;
+        LeanTouch.OnFingerUp += press;
+    }
+
+    void OnDisable()
+    {
+        LeanTouch.OnFingerDown -= press;
+        LeanTouch.OnFingerUp -= press;
+    }
+
+    void press(LeanFinger finger)
+    {
+        Pressed = !Pressed;
+    }
+}
